a8454e34d0137c3a987eaa2555602ce8681135be
Facilita Estudante <sprt.eduardo.client@gmail.com>
2026-04-10 20:34:30 -0300

    Refactor app data structure in inter.js

--- inter.js
+++ inter.js
@@ -1,34 +1,20 @@
 [
-    {
-        "app_name": "Inter Browser",
-        "id_app": "com.inter.browser",
-        "icon": "false",
-        "version_code": "17.0.1",
-        "description_app": "Um dos melhores organizadores feitos para você te acompanhar!!",
-        "release_notes": [
-            "O seu organizador na palma da mão!",
-            "Sistema robusto e seguro!",
-            "Sincronização em tempo real!"
-        ],
-        "download_url": "https://github.com/inter-estudios-developers/Facilita-Estudante/releases/download/v2.0.NAV/Phanton-Browser-V16.exe",
-        "web_site": false,
-        "license": false,
-        "notification_update": true
-    },
-    {
-        "app_name": "Inter Browser",
-        "id_app": "com.inter.browser",
-        "icon": "false",
-        "version_code": "17.0",
-        "description_app": "Um dos melhores organizadores feitos para você te acompanhar!!",
-        "release_notes": [
-            "O seu organizador na palma da mão!",
-            "Sistema robusto e seguro!",
-            "Sincronização em tempo real!"
-        ],
-        "download_url": "https://github.com/inter-estudios-developers/Facilita-Estudante/releases/download/v2.0.NAV/Phanton-Browser-V16.exe",
-        "web_site": false,
-        "license": false,
-        "notification_update": true
-    }
+  {
+    "app_name": "Inter Browser",
+    "id_app": "com.inter.browser",
+    "icon": "false",
+    "version_code": "17.0.1",
+    "description_app": "Um dos melhores organizadores...",
+    "release_notes": ["Nota 1", "Nota 2", "Nota 3"],
+    "download_url": "https://github.com/.../setup.exe",
+    "web_site": false,
+    "license": false,
+    "notification_update": true
+  },
+  {
+    "app_name": "Inter Browser",
+    "id_app": "com.inter.browser",
+    "version_code": "17.0",
+    ...
+  }
 ]
